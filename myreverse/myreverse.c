098d69d60c942f3cdd1216120efb6cb6b1507b75
Дмитрий <javier@Javiers.(none)>
2012-04-02 23:56:54 +0400

    Bug fixed

--- myreverse/myreverse.c
+++ myreverse/myreverse.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #define BUF_SZ 5
 
 int print_reversed(char* arr, int first, int last)
@@ -11,6 +13,8 @@ int print_reversed(char* arr, int first, int last)
   }
   ans[sz] = arr[last];
   sz++;
+  if (ans[sz-1] == 0)
+	sz--;	
   return write(1, ans, sz);
 }
 
@@ -18,19 +22,28 @@ int main()
 {
   int sz = 0, over = 0;
   char buf[BUF_SZ+1];
+  for (int i = 0; i <= BUF_SZ; i++)
+	buf[i] = 0;
   int read_status; 
   while (1)
   {
-    read_status = read(0, buf+sz, BUF_SZ-sz + 1);
+    read_status = read(0, buf+sz, BUF_SZ - sz + 1);
     if (read_status < 0)
       return -1;
-    sz = BUF_SZ+1;
-    int pos=0;
+	if (read_status == 0)
+	{
+	  if (! over)
+        return print_reversed(buf, 0, sz);
+	}
+    sz += read_status;
+    int pos = 0;
     for (int i = 0; i < sz; i++)
-      if (buf[i]=='\n')
+    {
+      if (buf[i] == '\n')
       {
         if (! over)
  		{
+//		  printf("%d %d\n",pos,i);
           if (print_reversed(buf, pos, i) <= 0)
             return -1;
         }    
@@ -38,20 +51,24 @@ int main()
           over = 0;
         pos = i+1;
       }
-    if (read_status == 0)
-      return 0;
-    if (pos == 0)
-    {
-      sz = 0;
-      over = 1;
-    }
-    else
-    {
-       for (int j= pos; j < BUF_SZ+1; j++)
-          buf[j - pos] = buf[j];
-        sz = BUF_SZ + 1 - pos;        
-        over = 0;
-    }
+	}
+
+	if (sz == BUF_SZ + 1 && pos == 0)
+	{
+	  over = 1;
+	  sz = 0;
+	  for (int i = 0; i <= BUF_SZ; i++)
+		buf[i] = 0;
+	}
+	else
+	{
+		for (int i = pos; i < sz; i++)
+			buf[i - pos] = buf[i];
+		sz = sz - pos;
+		for (int i = sz; i < BUF_SZ + 1; i++)
+			buf[i] = 0;
+	}
+    
   }
   return 0;
 }
