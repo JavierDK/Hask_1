bbd5329750a17b4e7ef30635aeb2f8033ac0ab99
Дмитрий <javier@Javiers.(none)>
2012-06-28 18:25:14 +0400

    With own thread buckets

--- myalloc/myalloc.c
+++ myalloc/myalloc.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <pthread.h>
 #include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -6,6 +7,8 @@
 #include <sys/mman.h>
 
 #define THRESHOLD 64
+#define BIG_THREAD_MEM 5
+#define SMALL_THREAD_MEM 20
 #define MASK 32
 
 
@@ -24,28 +27,31 @@ typedef struct Queue
 typedef struct ThreadMem
 {
 	pid_t tid;
-	BucketNode *list;
+	Queue alloc;
+	BucketNode small[SMALL_THREAD_MEM], large[BIG_THREAD_MEM];
 	struct ThreadMem* next;
 } ThreadMem;
 
 Queue smallB, largeB;
 ThreadMem *allocMem;
+pthread_mutex_t threadListMutex;
 
 BucketNode* createBN()
 {
-		BucketNode* res = (BucketNode*) mmap(NULL, sizeof(BucketNode), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+		BucketNode* res = (BucketNode*) mmap(NULL, sizeof(BucketNode), PROT_READ | PROT_WRITE, MAP_PRIVATE, -1, 0);
 		return res;		
 }
 
 ThreadMem* createTM()
 {
-		ThreadMem* res = (ThreadMem*) mmap(NULL, sizeof(ThreadMem), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+		ThreadMem* res = (ThreadMem*) mmap(NULL, sizeof(ThreadMem), PROT_READ | PROT_WRITE, MAP_PRIVATE, -1, 0);
 		return res;		
 }
 
 ThreadMem* threadFind(pid_t iam)
 {
 	ThreadMem* posT, *prevT = NULL;
+	pthread_mutex_lock(&threadListMutex); 
 	posT = allocMem;
 	while (posT != NULL)
 	{
@@ -54,16 +60,24 @@ ThreadMem* threadFind(pid_t iam)
 		prevT = posT;
 		posT = posT -> next;		
 	}
+	pthread_mutex_unlock(&threadListMutex); 
 	if (posT = NULL)
 	{
 		ThreadMem *new = createTM();
 		new -> tid = iam;
-		new -> list = NULL;
-		new -> next = NULL; 
+		new -> alloc.first = NULL;
+		new -> alloc.last = NULL;
+		new -> next = NULL;
+		for (int i = 0; i < SMALL_THREAD_MEM; i++)
+			new -> small[i].addr = NULL;			
+		for (int i = 0; i < BIG_THREAD_MEM; i++)
+			new -> large[i].addr = NULL;		
+		pthread_mutex_lock(&threadListMutex); 
 		if (prevT == NULL)
 			allocMem = new;
 		else
 			prevT -> next = new;
+		pthread_mutex_unlock(&threadListMutex); 
 		return new;
 	}
 	else
@@ -72,8 +86,29 @@ ThreadMem* threadFind(pid_t iam)
 
 void* getSmall(int size)
 {
-	pid_t iam = gettid();
+	pid_t iam = pthread_self();
 	ThreadMem *pos = threadFind(iam);
+	for (int i = 0; i < SMALL_THREAD_MEM; i++)
+		if (pos -> small[i].addr != NULL && pos -> small[i].size >= size)
+		{
+			BucketNode *new = createBN();
+			new -> addr = pos -> small[i].addr;
+			new -> size = pos -> small[i].size;
+			new -> next = NULL;
+			new -> prev = NULL;
+			if (pos -> alloc.first == NULL)
+			{
+				pos -> alloc.first = new;
+				pos -> alloc.last = new;
+			}
+			else
+			{
+				pos -> alloc.last -> next = new;
+				new -> prev = pos -> alloc.last;
+				pos -> alloc.last = new;
+			}
+			return new -> addr;			
+		}
 	return NULL;
 }
 
@@ -96,6 +131,16 @@ void* malloc(size_t size)
 void free(void *ptr)
 {
 	return;
+}
+
+void _init()
+{
+	pthread_mutex_init(&threadListMutex, 0);
+}
+
+void _fini()
+{
+	pthread_mutex_destroy(&threadListMutex);
 }
 
 int main()
